b8e0b17799dc1ee6548b11b63edddaa69189da93
ManuBarroso <manubargon@gmail.com>
2018-04-23 12:07:18 +0200

    More style adjustments
    Adding information about arguments
    adding an starting _ for local constants

--- entertainment_center.py
+++ entertainment_center.py
@@ -8,42 +8,42 @@ This file is made to be executed with no parameters.
 import media
 import fresh_tomatoes
 
-TOY_STORY = media.Movie("Toy Story",
+_TOY_STORY = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
                         "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-AVATAR = media.Movie("Avatar",
+_AVATAR = media.Movie("Avatar",
                      "A marine on an alien planet",
                      "https://upload.wikimedia.org/wikipedia/sco/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 
-AMELIE = media.Movie("Amelie",
+_AMELIE = media.Movie("Amelie",
                      "The story of a girl and the small important things in life",
                      "https://upload.wikimedia.org/wikipedia/en/5/53/Amelie_poster.jpg",
                      "https://www.youtube.com/watch?v=HUECWi5pX7o")
 
 
-RATATOUILLE = media.Movie("Ratatouille",
+_RATATOUILLE = media.Movie("Ratatouille",
                           "Storyline",
                           "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
                           "https://www.youtube.com/watch?v=c3sBBRxDAqk")
 
-MIDNIGHT_IN_PARIS = media.Movie("Midnight in Paris",
+_MIDNIGHT_IN_PARIS = media.Movie("Midnight in Paris",
                                 "Storyline",
                                 "http://t3.gstatic.com/images?q=tbn:ANd9GcTk3ssys2bKM5-U6XMgvoD8yVoS5Io2YKg_1xA6x6GA8mKuuqID",
                                 "https://www.youtube.com/watch?v=FAfR8omt-CY")
 
-HUNGER_GAMES = media.Movie("The Hunger Games",
+_HUNGER_GAMES = media.Movie("The Hunger Games",
                            "Storyline",
                            "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
                            "https://www.youtube.com/watch?v=mfmrPu43DF8")
 
 #default_movie = media.Movie("not a film")
 
-MOVIES = [TOY_STORY, AVATAR, AMELIE, RATATOUILLE, MIDNIGHT_IN_PARIS, HUNGER_GAMES]
-fresh_tomatoes.open_movies_page(MOVIES)
+_MOVIES = [_TOY_STORY, _AVATAR, _AMELIE, _RATATOUILLE, _MIDNIGHT_IN_PARIS, _HUNGER_GAMES]
+fresh_tomatoes.open_movies_page(_MOVIES)
 
 #print (media.Movie.__name__)
 #print (media.Movie.__module__)
--- media.py
+++ media.py
@@ -14,6 +14,16 @@ class Movie():
                  movie_storyline="No storyline found",
                  poster_image="https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg",
                  trailer_youtube="https://www.youtube.com/watch?v=IYnsfV5N2n8"):
+        """Constructor
+
+        Arguments:
+            movie_title: the name of the movie
+            movie_storyline: the description of the story. By default: "No storyline found",
+            poster_image: the image of the main poster of the movie. By default: a "no image available" image
+            trailer_youtube: the trailer of the movie in youtube. It has to be the complete url. By default: ASDF Movie 1
+
+            TODO: make a default video with 20 seconds of "no trailer available"
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
